a034400b52d445c1307d39ba682e58bfadd24a7a
Ryan Liptak <squeek502@hotmail.com>
2017-12-08 03:52:03 -0800

    Add session tracking and xp/min display

---
+++ d2info/session.lua
@@ -0,0 +1,57 @@
+local constants = require('d2info.constants')
+
+local Session = {}
+Session.__index = Session
+
+function Session.new(startExp, startLevel)
+  local self = setmetatable({}, Session)
+  self.startTime = os.time()
+  self.duration = 0
+  self.startExp = startExp
+  self.exp = startExp
+  self.startLevel = startLevel
+  self.level = startLevel
+  return self
+end
+
+function Session:update(exp, level)
+  self.exp = exp
+  self.level = level
+end
+
+function Session:incrementDuration(inc)
+  if inc == nil then inc = 1 end
+  self.duration = self.duration + inc
+end
+
+local function expPerMin(expGained, duration)
+  local mins = duration / 60
+  if mins == 0 then return 0 end
+  return expGained / mins
+end
+
+function Session:realTimeExpPerMin()
+  return expPerMin(self.exp - self.startExp, os.time() - self.startTime)
+end
+
+function Session:durationExpPerMin()
+  return expPerMin(self.exp - self.startExp, self.duration)
+end
+
+local function secondsToNextLevel(exp, level, expGained, duration)
+  if level == 99 then return nil end
+  local expNeeded = constants.experience[level+1] - exp
+  local expPerMin = expPerMin(expGained, duration)
+  local minsNeeded = expNeeded / expPerMin
+  return minsNeeded * 60
+end
+
+function Session:realTimeToNextLevel()
+  return secondsToNextLevel(self.exp, self.level, self.exp - self.startExp, os.time() - self.startTime)
+end
+
+function Session:gameTimeToNextLevel()
+  return secondsToNextLevel(self.exp, self.level, self.exp - self.startExp, self.duration)
+end
+
+return Session
--- d2info/utils.lua
+++ d2info/utils.lua
@@ -16,4 +16,43 @@ function utils.tableMerge(a, b)
   return merged
 end
 
+-- Write to the console by overwriting the line using CR
+-- Uses spaces to cover up the last message when needed
+local lastLen = 0
+function utils.consoleWrite(msg)
+  local len = #msg
+  io.write('\r')
+  io.write(msg)
+  if len < lastLen then
+    io.write(string.rep(' ', lastLen-len))
+  end
+  lastLen = len
+end
+
+function utils.friendlyNumber(num)
+  if num > 1000000 then
+    return string.format("%0.2fm", num / 1000000)
+  elseif num > 1000 then
+    return string.format("%0.1fk", num / 1000)
+  end
+  return string.format("%u", num)
+end
+
+local secondsPerMin = 60
+local secondsPerHour = secondsPerMin * 60
+local secondsPerDay = secondsPerHour * 24
+function utils.friendlyTime(seconds, days)
+  if seconds == nil then
+    return "∞"
+  elseif days and seconds > secondsPerDay then
+    return string.format("%ud%02uh", math.floor(seconds / secondsPerDay), (seconds % secondsPerDay) / secondsPerHour)
+  elseif seconds > secondsPerHour*10 then
+    return string.format("%uh", math.floor(seconds / secondsPerHour))
+  elseif seconds > secondsPerHour then
+    return string.format("%uh%um", math.floor(seconds / secondsPerHour), (seconds % secondsPerHour) / secondsPerMin)
+  else
+    return string.format("%um%02us", math.floor(seconds / secondsPerMin), seconds % secondsPerMin)
+  end
+end
+
 return utils
