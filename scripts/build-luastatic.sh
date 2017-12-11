0b44664709e3c0fa079a2d77be97623113160cfc
Ryan Liptak <squeek502@hotmail.com>
2017-12-11 01:50:07 -0800

    Attempt to fix build-luastatic.sh on travis

--- scripts/build-luastatic.sh
+++ scripts/build-luastatic.sh
@@ -4,6 +4,9 @@
 # Should be executed from root d2info directory.
 # Resulting binary will be in `build/d2info.exe`.
 
+ROOT_DIR=$PWD
+BUILD_DIR=$ROOT_DIR/build
+
 rm -rf build
 mkdir build
 cd build
@@ -30,32 +33,32 @@ echo "=== Building Lua ==="
 echo
 cd lua-$LUA_VERSION
 make mingw CC=x86_64-w64-mingw32-gcc AR="x86_64-w64-mingw32-ar rcu"
-cp src/liblua.a .. || exit 1
-cd ..
+cp src/liblua.a $BUILD_DIR || exit 1
+cd $BUILD_DIR
 
 echo
 echo "=== Building memreader ==="
 echo
 cd memreader
-cmake -DCMAKE_TOOLCHAIN_FILE=../../scripts/toolchain-mingw.cmake -DLUA_LIBRARIES=../liblua.a -DLUA_INCLUDE_DIR=../lua-$LUA_VERSION/src .
+cmake -DCMAKE_TOOLCHAIN_FILE=$ROOT_DIR/scripts/toolchain-mingw.cmake -DLUA_LIBRARIES=$BUILD_DIR/liblua.a -DLUA_INCLUDE_DIR=$BUILD_DIR/lua-$LUA_VERSION/src .
 make
-cp libmemreader.a .. || exit 1
-cd ..
+cp libmemreader.a $BUILD_DIR || exit 1
+cd $BUILD_DIR
 
 echo
 echo "=== Building sleep ==="
 echo
 cd sleep
-cmake -DCMAKE_TOOLCHAIN_FILE=../../scripts/toolchain-mingw.cmake -DLUA_LIBRARIES=../liblua.a -DLUA_INCLUDE_DIR=../lua-$LUA_VERSION/src .
+cmake -DCMAKE_TOOLCHAIN_FILE=$ROOT_DIR/scripts/toolchain-mingw.cmake -DLUA_LIBRARIES=$BUILD_DIR/liblua.a -DLUA_INCLUDE_DIR=$BUILD_DIR/lua-$LUA_VERSION/src .
 make
-cp libsleep.a .. || exit 1
-cd ..
+cp libsleep.a $BUILD_DIR || exit 1
+cd $BUILD_DIR
 
 echo
 echo "=== Copying d2info sources ==="
 echo
-cp -r ../d2info .
-cp ../d2info.lua .
+cp -r $ROOT_DIR/d2info $BUILD_DIR
+cp $ROOT_DIR/d2info.lua $BUILD_DIR
 
 echo
 echo "=== Building d2info.exe ==="
@@ -63,4 +66,4 @@ echo
 CC=x86_64-w64-mingw32-gcc luastatic d2info.lua d2info/*.lua liblua.a libmemreader.a libsleep.a /usr/x86_64-w64-mingw32/lib/libversion.a /usr/x86_64-w64-mingw32/lib/libpsapi.a -Ilua-$LUA_VERSION/src
 strip d2info.exe || exit 1
 
-cd ..
+cd $ROOT_DIR
