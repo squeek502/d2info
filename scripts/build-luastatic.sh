ec9796ec965b5f6880150afe3ea28e3cdae40cfa
Ryan Liptak <squeek502@hotmail.com>
2017-12-11 01:37:18 -0800

    Add exits to build-luastatic.sh when something fails

--- scripts/build-luastatic.sh
+++ scripts/build-luastatic.sh
@@ -18,19 +18,19 @@ curl https://www.lua.org/ftp/lua-$LUA_VERSION.tar.gz | tar xz
 echo
 echo "=== Downloading memreader ==="
 echo
-git clone https://github.com/squeek502/memreader.git
+git clone https://github.com/squeek502/memreader.git || exit 1
 
 echo
 echo "=== Downloading sleep ==="
 echo
-git clone https://github.com/squeek502/sleep.git
+git clone https://github.com/squeek502/sleep.git || exit 1
 
 echo
 echo "=== Building Lua ==="
 echo
 cd lua-$LUA_VERSION
 make mingw CC=x86_64-w64-mingw32-gcc AR="x86_64-w64-mingw32-ar rcu"
-cp src/liblua.a ..
+cp src/liblua.a .. || exit 1
 cd ..
 
 echo
@@ -39,7 +39,7 @@ echo
 cd memreader
 cmake -DCMAKE_TOOLCHAIN_FILE=../../scripts/toolchain-mingw.cmake -DLUA_LIBRARIES=../liblua.a -DLUA_INCLUDE_DIR=../lua-$LUA_VERSION/src .
 make
-cp libmemreader.a ..
+cp libmemreader.a .. || exit 1
 cd ..
 
 echo
@@ -48,7 +48,7 @@ echo
 cd sleep
 cmake -DCMAKE_TOOLCHAIN_FILE=../../scripts/toolchain-mingw.cmake -DLUA_LIBRARIES=../liblua.a -DLUA_INCLUDE_DIR=../lua-$LUA_VERSION/src .
 make
-cp libsleep.a ..
+cp libsleep.a .. || exit 1
 cd ..
 
 echo
@@ -61,6 +61,6 @@ echo
 echo "=== Building d2info.exe ==="
 echo
 CC=x86_64-w64-mingw32-gcc luastatic d2info.lua d2info/*.lua liblua.a libmemreader.a libsleep.a /usr/x86_64-w64-mingw32/lib/libversion.a /usr/x86_64-w64-mingw32/lib/libpsapi.a -Ilua-$LUA_VERSION/src
-strip d2info.exe
+strip d2info.exe || exit 1
 
 cd ..
